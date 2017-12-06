lineage-next/android_build_make

core: Include Lineage pathmap

core/pathmap.mk
@@ -169,3 +169,6 @@ FRAMEWORKS_DATA_BINDING_SUBDIRS := \
 FRAMEWORKS_DATA_BINDING_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/data-binding/,$(FRAMEWORKS_DATA_BINDING_SUBDIRS))
 
+ifneq ($(LINEAGE_BUILD),)
+-include $(TOPDIR)vendor/lineage/build/core/pathmap.mk
+endif

ReaperRoms/build_make

Change-Id: Ie25dee383cc5bc9bb6390cff2cb2460d526d80b6

core/pathmap.mk
@@ -99,3 +99,7 @@ FRAMEWORKS_BASE_SUBDIRS := \
 #
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
+
+ifneq ($(LINEAGE_BUILD),)
+-include $(TOPDIR)vendor/reaper/build/core/pathmap.mk
+endif